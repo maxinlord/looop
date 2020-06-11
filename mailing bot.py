bd0cf36e28c8397c2bfa8697429b75cd37de9fa2
maxinlord <65973902+maxinlord@users.noreply.github.com>
2020-06-11 19:16:06 +0300

    Add files via upload

--- mailing bot.py
+++ mailing bot.py
@@ -2,8 +2,6 @@ import telebot
 import config2
 from telebot import types
 
-
-
 bot = telebot.TeleBot(config2.TOKEN)
 
 markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -20,13 +18,14 @@ markup1.row(itembtnc)
 @bot.message_handler(commands=['start', 'help'])
 @bot.edited_message_handler(content_types=['text'])
 def welcome(message):
-    bot.send_message(message.chat.id,'Здравствуйте вас приветствует бот для рассылки поста по чатам. Нажмите соответствующий пункт меню для создания новой рассылки',reply_markup=markup1 )
+    bot.send_message(message.chat.id,
+                     'Здравствуйте вас приветствует бот для рассылки поста по чатам. Нажмите соответствующий пункт меню для создания новой рассылки',
+                     reply_markup=markup1)
 
 
 @bot.message_handler(content_types=['text'])
 def otvet(message):
-    bot.send_message(message.chat.id,'Упс... пока ничего нет)')
-
+    bot.send_message(message.chat.id, 'Упс... пока ничего нет)')
 
 
 bot.polling(timeout=60)
